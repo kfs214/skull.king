9db0b20e3db1663bfe141e20eea3b0bfad9fddfe
牛島一樹 <kazuki@macbook.local>
2020-07-25 17:27:14 +0900

    a became like a.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -20,6 +20,7 @@
         <h3>動物で、繋がる。</h3>
         <img src="{{ asset('/links/Ikkonzome.jpeg') }}">
         <p>動物占いのアプリです。チーム機能や掲示板機能があり、自分の診断結果を見るだけでなく、身の回りの人を登録して並べ替え表示を行ったり、掲示板で他のユーザーと交流したりできます。</p>
+        <p><u>Ikkonzomeを使ってみる</u></p>
       </div>
     </a>
 
@@ -29,6 +30,7 @@
         <h3>スカルキング得点計算ツール</h3>
         <img src="{{ asset('/links/ROkusho.jpeg') }}">
         <p>スカルキングの得点計算ツールです。各ラウンドでの「予想勝ち数」と「勝ち数」を入力することで各プレイヤーの総合成績を計算します。簡易計算にも対応し、単一ラウンドでの得点計算も可能です。</p>
+        <p><u>ROkushoを使ってみる</u></p>
       </div>
     </a>
 
@@ -38,6 +40,7 @@
         <h3>ブログ投稿支援ツール</h3>
         <img src="{{ asset('/links/HAjizome.jpeg') }}">
         <p>ふんどし王子事典でも利用している投稿支援ツールです。見出しや本文を定まった形式で入力するとHTMLタグを付加します。</p>
+        <p><u>HAjizomeを使ってみる</u></p>
       </div>
     </a>
   </div>
@@ -46,7 +49,7 @@
       <div class="container">
         <p>
           {{ __('Please use this application at your own risk.') }}<br>
-          {{ __('Send feedback:') }}<a href="https://kfs214.net/articles/425#006" target="_blank">kfs214</a>
+          {{ __('Send feedback:') }}<a href="https://kfs214.net/articles/425#006" target="_blank"><u>kfs214</u></a>
         </p>
       </div>
   </div>
